2468b9fe3964486de6e111baf2a05a8ddf5cfc6c
sq1533 <618356@naver.com>
2024-07-12 15:56:13 +0900

    feat:조회 페이지 UI 변경

--- pages/1lookup.py
+++ pages/1lookup.py
@@ -1,5 +1,6 @@
 import json
 import streamlit as st
+from streamlit_extras.row import row
 from customs.custom import css
 #사이드바 제거
 st.markdown(css, unsafe_allow_html=True)
@@ -7,7 +8,10 @@ st.markdown(css, unsafe_allow_html=True)
 #실시간 알람 불러오기
 with open('C:\\Users\\USER\\ve_1\\alarmCapture\\db\\info_.json','r',encoding="UTF-8") as f:
     DF = json.load(f)
-mid = st.text_input("MID조회(입력 후 Enter)")
+
+bady1 = row(1, vertical_align="top")
+bady2 = row(2, vertical_align="top")
+mid = bady2.text_input("MID조회(입력 후 Enter)")
 look = [i for i in range(len(DF)) if DF[i]['mid']==mid]
 
 if mid:
