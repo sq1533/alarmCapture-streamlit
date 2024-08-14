d76d9048d184880221dfa10234a2dfedaae0b461
sq1533 <618356@naver.com>
2024-08-14 18:10:37 +0900

    feat: 미사용 파일 제거

---
+++ customs/custom.py
@@ -0,0 +1,8 @@
+css = '''
+<style>
+    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 2rem;}
+    header {visibility: hidden;}
+    .streamlit-footer {display: none;}
+    .st-emotion-cache-uf99v8 {display: none;}
+</style>
+'''
